d1d940c3ede0a35e6703605244b00019478ebc42
Shiham <s36651786@gmail.com>
2024-10-19 12:42:22 +0600

    Todo Card Created

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,7 +1,15 @@
 const TodoCard = () => {
 	return (
-		<div>
-			<h1>Todo Card</h1>
+		<div className="bg-white rounded-md flex justify-between items-center p-3">
+			<input type="checkbox" name="" id="" />
+			<p className="font-semibold">Todo Title</p>
+			<p>Todo Created Time</p>
+			<p>Short Description</p>
+
+			<div className="flex space-x-4">
+				<button>Delete</button>
+				<button>Edit</button>
+			</div>
 		</div>
 	)
 }
--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -1,7 +1,17 @@
+import TodoCard from "./TodoCard"
+
 const TodoContainer = () => {
 	return (
 		<div>
-			<h1>Todo Container</h1>
+			<div>
+				<button>add Todo</button>
+				<button>Filter</button>
+			</div>
+			<div className="bg-red-500 w-full h-[500px] rounded-md p-10 space-y-4">
+				<TodoCard />
+				<TodoCard />
+				<TodoCard />
+			</div>
 		</div>
 	)
 }
