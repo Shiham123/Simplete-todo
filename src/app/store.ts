d86859b4e9df3e69f6b36b3fa441ac9d52f86431
Shiham <s36651786@gmail.com>
2024-10-19 11:29:14 +0600

    redux type validation and also set todo slice setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
+import TodoReducer from "./features/todoSlice"
 
 const store = configureStore({
-	reducer: {},
+	reducer: {
+		todos: TodoReducer,
+	},
 })
 
 // ! typescript type initialization
