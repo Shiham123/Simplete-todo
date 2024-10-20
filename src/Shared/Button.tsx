90f103c73a90da2c3173abbf686e7341c880e3b8
Shiham <s36651786@gmail.com>
2024-10-20 08:27:18 +0600

    button tsx used

--- src/Shared/Button.tsx
+++ src/Shared/Button.tsx
@@ -1,7 +1,12 @@
 const Button = () => {
 	return (
-		<div>
-			<h1>Button Container</h1>
+		<div className="flex justify-between items-center">
+			<button className="bg-black px-4 py-2 text-white capitalize text-2xl rounded-lg hover:bg-transparent hover:text-black border-2 border-black transition-colors duration-300">
+				Add Todo
+			</button>
+			<button className="bg-black px-4 py-2 text-white capitalize text-2xl rounded-lg hover:bg-transparent hover:text-black border-2 border-black transition-colors duration-300">
+				Filter
+			</button>
 		</div>
 	)
 }
--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -1,17 +1,11 @@
+import Button from "../Shared/Button"
 import TodoCard from "./TodoCard"
 
 const TodoContainer = () => {
 	return (
 		<div className="space-y-5">
 			{/* Todo Button Here */}
-			<div className="flex justify-between items-center">
-				<button className="bg-black px-4 py-2 text-white capitalize text-2xl rounded-lg hover:bg-transparent hover:text-black border-2 border-black transition-colors duration-300">
-					add Todo
-				</button>
-				<button className="bg-black px-4 py-2 text-white capitalize text-2xl rounded-lg hover:bg-transparent hover:text-black border-2 border-black transition-colors duration-300">
-					Filter
-				</button>
-			</div>
+			<Button />
 
 			{/* todo Body Here */}
 			<div className="bg-red-500 w-full h-full rounded-md p-10 space-y-4">
