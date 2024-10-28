871e7502c673a19a512105b866925495bfe96770
Shiham <s36651786@gmail.com>
2024-10-28 07:33:51 +0600

    express morgan and localserver working

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,26 @@
 require("dotenv").config()
+const {MongoClient, ServerApiVersion} = require("mongodb")
+
+// require of the backend features
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
+const cors = require("cors")
+const morgan = require("morgan")
 
-console.log("hello there")
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+const mongodbUrl = "mongodb://localhost:27017/"
+
+const client = new MongoClient(mongodbUrl, {serverApi: ServerApiVersion.v1})
+
+app.get("/", (req, res) => {
+	res.send("hello world")
+})
+
+app.listen(port, () => {
+	// eslint-disable-next-line no-console
+	console.log(`http://localhost:${port}`)
+})
