e7b66018d48aeecbf43c8f09e057b18d78472e80
Shiham <s36651786@gmail.com>
2024-10-28 07:15:19 +0600

    eslint setup

--- Backend/eslint.config.mjs
+++ Backend/eslint.config.mjs
@@ -1,7 +1,9 @@
 import globals from "globals"
+import pluginJs from "@eslint/js"
 
 export default [
 	{files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-	{languageOptions: {globals: globals.browser}},
-	{rules: {"no-console": ["error"]}},
+	{languageOptions: {globals: globals.node}},
+	pluginJs.configs.recommended,
+	{rules: {"no-console": ["error"], "no-unused-vars": "warn"}},
 ]
