ac6e1e584a1c49d345eb48569e21a32d448a80ae
Shiham <s36651786@gmail.com>
2024-10-28 08:04:19 +0600

    we will go with express and another package

--- Backend/index.js
+++ Backend/index.js
@@ -1,62 +1,29 @@
 require("dotenv").config()
-const http = require("http")
-const url = require("url")
 const {MongoClient, ServerApiVersion} = require("mongodb")
 
-// Port from environment or default to 5000
+// Require files here
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan") // Import Morgan
+const app = express()
 const port = process.env.PORT || 5000
 
-// MongoDB setup
 const mongodbUrl = "mongodb://localhost:27017/"
 const client = new MongoClient(mongodbUrl, {serverApi: ServerApiVersion.v1})
 
-// Basic CORS setup
-const enableCors = (res) => {
-	res.setHeader("Access-Control-Allow-Origin", "*")
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type")
-}
+// Use Morgan middleware for logging
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev")) // Use 'dev' for concise output
 
-// Request handler function
-const requestHandler = async (req, res) => {
-	const parsedUrl = url.parse(req.url, true)
-	const {pathname} = parsedUrl
+// Handle GET requests
+app.get("/", (req, res) => {
+	res.send("hello world")
+})
 
-	// Enable CORS for all responses
-	enableCors(res)
+// Handle POST requests
 
-	// Root route
-	if (pathname === "/" && req.method === "GET") {
-		res.writeHead(200, {"Content-Type": "text/plain"})
-		res.end("Hello World")
-		return
-	}
-
-	// Handle 404 Not Found for unknown routes
-	res.writeHead(404, {"Content-Type": "application/json"})
-	res.end(JSON.stringify({message: "Route not found"}))
-}
-
-// Create the server
-const server = http.createServer(requestHandler)
-
-// Start the server and connect to MongoDB
-const startServer = () => {
-	client
-		.connect()
-		.then(() => {
-			// eslint-disable-next-line no-console
-			console.log("Connected to MongoDB")
-
-			server.listen(port, () => {
-				// eslint-disable-next-line no-console
-				console.log(`Server running at http://localhost:${port}`)
-			})
-		})
-		.catch((err) => {
-			// eslint-disable-next-line no-console
-			console.error("Failed to connect to MongoDB", err)
-		})
-}
-
-startServer()
+app.listen(port, () => {
+	// eslint-disable-next-line no-console
+	console.log(`http://localhost:${port}`)
+})
