e9f60c7212b2646e110e6c5daf500f89d5d8292d
Shiham <s36651786@gmail.com>
2024-11-02 10:24:11 +0600

    sss

--- Fronted/src/app/api/api.ts
+++ Fronted/src/app/api/api.ts
@@ -2,5 +2,6 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
 const baseApi = createApi({
 	reducerPath: "baseApi",
-	baseQuery: fetchBaseQuery({baseUrl: ""}),
+	baseQuery: fetchBaseQuery({baseUrl: "http://localhost:5000"}),
+	endpoints: () => {},
 })
